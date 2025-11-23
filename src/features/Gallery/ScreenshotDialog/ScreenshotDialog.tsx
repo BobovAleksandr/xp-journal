975ffd3c4b66a5de8b52d3a38baa0935cba9206d
Александр Бобов <bobov.aleksandr.fr@yandex.ru>
2025-11-23 21:03:22 +0500

    feat - Добавил диалог для просмотрах полноразмерных скриншотов с каруселью

---
+++ src/features/Gallery/ScreenshotDialog/ScreenshotDialog.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import Image from "next/image";
+import cn from "classnames";
+import IconButton from "@/shared/components/IconButton/IconButton";
+import CloseIcon from "@/shared/assets/xmark.svg";
+import { buildImageUrl } from "@/shared/utils/buildImageUrl";
+import styles from "./ScreenshotDialog.module.scss";
+import IconRight from "@/shared/assets/chevron-right.svg";
+import IconLeft from "@/shared/assets/chevron-left.svg";
+
+type Props = {
+  isOpen: boolean;
+  imageId: string | null;
+  gameName?: string;
+  className?: string;
+  onClose: () => void;
+  onNext: () => void;
+  onPrev: () => void;
+};
+
+export default function ScreenshotDialog({
+  isOpen,
+  imageId,
+  gameName,
+  className,
+  onNext,
+  onPrev,
+  onClose,
+}: Props) {
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (!dialog) return;
+
+    if (isOpen && !dialog.open) {
+      dialog.showModal();
+    } else if (!isOpen && dialog.open) {
+      dialog.close();
+    }
+  }, [isOpen]);
+
+  if (!mounted || !isOpen) return null;
+
+  return createPortal(
+    <dialog
+      ref={dialogRef}
+      className={cn(styles.dialog, className)}
+      onCancel={onClose}
+    >
+      <header className={styles.dialog_header}>
+        <IconButton
+          icon={CloseIcon}
+          onClick={onClose}
+          variant="large"
+          className={styles.dialog_close}
+        />
+      </header>
+
+      {imageId && (
+        <div className={styles.dialog_image_container}>
+          <IconButton
+            icon={IconLeft}
+            onClick={onPrev}
+            variant="large"
+            className={styles.dialog_image_button}
+          />
+          <Image
+            src={buildImageUrl(imageId, "1080p")}
+            alt={`Скриншот ${gameName}`}
+            width={1440}
+            height={810}
+            className={styles.dialog_image}
+          />
+          <IconButton
+            icon={IconRight}
+            onClick={onNext}
+            variant="large"
+            className={styles.dialog_image_button}
+          />
+        </div>
+      )}
+    </dialog>,
+    document.body
+  );
+}
--- src/widgets/Screenshots/Screenshots.tsx
+++ src/widgets/Screenshots/Screenshots.tsx
@@ -3,7 +3,9 @@
 import PageSection from "@/shared/components/PageSection/PageSection";
 import styles from "./Screenshots.module.scss";
 import { TClientScreenshot } from "@/entities/game/model/types";
-import Screenshot from "@/entities/gallery/Screenshot/Screenshot";
+import Screenshot from "@/features/Gallery/Screenshot/Screenshot";
+import ScreenshotDialog from "@/features/Gallery/ScreenshotDialog/ScreenshotDialog";
+import { useState } from "react";
 
 type ScreenshotsProps = {
   screenshots: TClientScreenshot[];
@@ -11,20 +13,50 @@ type ScreenshotsProps = {
 };
 
 const Screenshots = ({ screenshots, gameName }: ScreenshotsProps) => {
+  const [activeImageIndex, setActiveImageIndex] = useState<number | null>(null);
+
+  const handleChangeScreenshot = (index: number) => {
+    setActiveImageIndex(index);
+  };
+
+  const isOpen = activeImageIndex !== null;
+
   return (
-    <PageSection heading="Скриншоты">
-      <ul className={styles.screenshots}>
-        {screenshots.map((screen) => (
-          <li key={screen.id}>
-            <Screenshot
-              onClick={() => {}}
-              imageId={screen.imageId}
-              alt={`Скриншот ${gameName}`}
-            />
-          </li>
-        ))}
-      </ul>
-    </PageSection>
+    <>
+      <PageSection heading="Скриншоты">
+        <ul className={styles.screenshots}>
+          {screenshots.map((screen, index) => (
+            <li key={screen.id}>
+              <Screenshot
+                imageId={screen.imageId}
+                alt={`Скриншот ${gameName}`}
+                onClick={() => {
+                  handleChangeScreenshot(index);
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </PageSection>
+      <ScreenshotDialog
+        isOpen={isOpen}
+        imageId={isOpen ? screenshots[activeImageIndex].imageId : null}
+        gameName={gameName}
+        onClose={() => setActiveImageIndex(null)}
+        onNext={() =>
+          setActiveImageIndex((prev) =>
+            prev === null ? 0 : (prev + 1) % screenshots.length
+          )
+        }
+        onPrev={() =>
+          setActiveImageIndex((prev) =>
+            prev === null
+              ? 0
+              : (prev - 1 + screenshots.length) % screenshots.length
+          )
+        }
+      />
+    </>
   );
 };
 
