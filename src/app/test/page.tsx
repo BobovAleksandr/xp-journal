39324878f87a58359fb0028a1289379ced68e256
Александр Бобов <bobov.aleksandr.fr@yandex.ru>
2025-12-27 21:28:42 +0500

    fix - cleared test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import GamesFilter from "@/features/GamesFilter/ui/GamesFilter/GamesFilter";
-import Filter from "@/shared/components/Filter/ui/Filter/Filter";
-import FilterItem from "@/shared/components/Filter/ui/FilterItem/FilterItem";
 
 const TestPage = () => {
   return (
     <>
-      <GamesFilter />
+      test
     </>
   );
 }
